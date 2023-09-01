84dcab961eb51d91699f89e7015fb76c5a3628d0
Pavel Machek <pavel@ucw.cz>
2023-09-02 00:16:47 +0200

    Provide fake functions for compatibility. Now breaks at missing delme.mtar

--- spacew.js
+++ spacew.js
@@ -1,6 +1,30 @@
 /* original openstmap.js */
 
 Bangle = {};
+Bangle.setGPSPower = print;
+Bangle.loadWidgets = print;
+Bangle.drawWidgets = print;
+WIDGETS = [];
+g = Graphics.createSDL(1024, 768, 8);
+
+function sdl_poll() {
+    e = g.getPixel(0, 0);
+    if (e) {
+	type = g.getPixel(1, 0);
+	switch(type) {
+	case 2: print("...key down", g.getPixel(2, 0)); break;
+	case 3: print("...key up"); break;
+	case 4: print("...move"); break;
+	case 5: print("...mouse down", g.getPixel(5,0), g.getPixel(6.0)); break;
+	case 6: print("...mouse up"); break;
+	case 12: print("...exit"); break;
+	default: print("...type:", type); break;
+	}
+    }
+}
+
+print("Test being loaded");
+setInterval(sdl_poll, 10);
 
 /* OpenStreetMap plotting module.
 
