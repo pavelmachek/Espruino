c36a11973b625e5ed3b24e5a751cff411a3cc938
Pavel Machek <pavel@ucw.cz>
2024-11-18 23:03:56 +0100

    Split touch test, make it include sdl.

--- poke.js
+++ poke.js
@@ -1,11 +1,11 @@
 
 print("Hello world");
 
-print(peek8(0x1234));
-poke8(0xdead, 0x42);
+//print(peek8(0x1234));
+//poke8(0xdead, 0x42);
 
-eval("a=1");
-print("a=", a);
+//eval("a=1");
+//print("a=", a);
 
-print(require("Storage").read("poke.js"))
+//print(require("Storage").read("poke.js"))
 print(require("fs").readFile("poke.js"))
