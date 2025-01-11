13cada8c10b3cd8c519125ec5d900603aeb6fb2e
Pavel Machek <pavel@ucw.cz>
2025-01-11 15:48:19 +0100

    hacks: some javascripts I was playing with

---
+++ poke.js
@@ -0,0 +1,11 @@
+
+print("Hello world");
+
+//print(peek8(0x1234));
+//poke8(0xdead, 0x42);
+
+//eval("a=1");
+//print("a=", a);
+
+//print(require("Storage").read("poke.js"))
+print(require("fs").readFile("poke.js"))
