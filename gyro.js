1375cd574e10dc796ca8d793d7fbee2e92f5561a
Pavel Machek <pavel@ucw.cz>
2025-10-26 22:11:53 +0100

    gyro: integrate values

--- gyro.js
+++ gyro.js
@@ -1,3 +1,13 @@
 eval(require("fs").readFile("sdl.js"));
 
-Bangle.on('gyro', print);
+let x = 0, y = 0, z = 0;
+
+function integrate(v) {
+    let step = 1/20;
+    x += step * v.x;
+    y += step * v.y;
+    z += step * v.z;
+    print(x, y, z);
+}
+
+Bangle.on('gyro', integrate);
