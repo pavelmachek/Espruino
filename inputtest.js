ed05d1a75e52138073b96769278cf263de843d04
Pavel Machek <pavel@ucw.cz>
2026-01-12 12:45:51 +0100

    input: Add test code.

--- inputtest.js
+++ inputtest.js
@@ -7,3 +7,23 @@ while (1) {
   if (r)
     print(r);
 }
+
+g.clear();
+g.setColor(0, 0, 0);
+g.setFont("Vector", 20);
+g.drawString("Press buttons", 20, 100);
+g.flip();
+
+// button: 1,2,3  (BTN1, BTN2, BTN3)
+// state: 1 = pressed, 0 = released
+Bangle.on('button', function (button, state) {
+  console.log("Button", button, state ? "DOWN" : "UP");
+
+  g.clear();
+  g.drawString(
+    "BTN" + button + " " + (state ? "DOWN" : "UP"),
+    40, 110
+  );
+  g.flip();
+});
+
